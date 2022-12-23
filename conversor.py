1d0ec5507711419d6a5addbc6eb06ee70c894ed6
Oswaldo Hernández Ávila <108857178+420Avila@users.noreply.github.com>
2022-12-22 21:49:10 -0600

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,4 +1,12 @@
 cantidad = float(input("Ingrese la cantidad: "))
 
+conversiones = {
+    "pulgada": 2.54,
+    "centímetro": 1,
+    "pie": 30.48,
+    "metro": 100,
+    "libra": 0.45359237,
+    "kilogramo": 1
+}
 
 
